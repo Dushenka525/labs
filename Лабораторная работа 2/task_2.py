1b57067e9e097367c1770d39d0f890b6240b9b47
Dushenka525 <152962772+Dushenka525@users.noreply.github.com>
2023-12-15 14:56:22 +0300

    Update task_2.py

--- Лабораторная работа 2/task_2.py
+++ Лабораторная работа 2/task_2.py
@@ -3,10 +3,11 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
 month=0
-while money_capital+salary>=spend:
+while True:
+    if money_capital + salary < spend:
+        break
     money_capital=money_capital+salary-spend
     spend=spend*(1+increase)
-
     month+=1
 
 # TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
